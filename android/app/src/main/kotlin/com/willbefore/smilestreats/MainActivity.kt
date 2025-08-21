362f03e36abafa46ddb25e45b44b9ec043c44347
Nazibullah Noyon <nazibullahnoyon19.20@gmail.com>
2025-08-22 00:26:35 +0600

    stripe setup

--- android/app/src/main/kotlin/com/willbefore/smilestreats/MainActivity.kt
+++ android/app/src/main/kotlin/com/willbefore/smilestreats/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.willbefore.smilestreats
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
