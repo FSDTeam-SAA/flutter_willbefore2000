9ed7b811704876c7622389dc83113fecd940d94e
noyon-360 <nazibullahnoyon19.20@gmail.com>
2026-03-10 10:36:13 +0600

    fix: Correct package import paths and update `flutter_riverpod` and `firebase_core` dependencies.

--- test/core/services/geo_service_test.dart
+++ test/core/services/geo_service_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:smilestreats/core/services/geo_service.dart';
+import 'package:smilestreatsapp/core/services/geo_service.dart';
 
 void main() {
   final geoService = GeoService();
